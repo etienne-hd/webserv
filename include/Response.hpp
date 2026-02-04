e3e070d3e55193dc439ecd58289261c549bb782d
etienne-hd <hode.etienne@gmail.com>
2026-02-04 18:40:25 +0100

    Added: Response private attribut

--- include/Response.hpp
+++ include/Response.hpp
@@ -6,8 +6,17 @@
 /*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 18:32:18 by ehode             #+#    #+#             */
-/*   Updated: 2026/02/04 18:32:22 by ehode            ###   ########.fr       */
+/*   Updated: 2026/02/04 18:39:55 by ehode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-class Response {};
+#include "Headers.hpp"
+
+#include <string>
+
+class Response {
+	private:
+		int			_status_code;
+		std::string _content;
+		Headers		_headers;
+};
