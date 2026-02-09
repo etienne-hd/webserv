a7aed099d95a7a27c8e725e92adba749c3cdd0d2
etienne-hd <hode.etienne@gmail.com>
2026-02-09 04:46:11 +0100

    Added: Client prototype

---
+++ include/Client.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/09 04:44:28 by ehode             #+#    #+#             */
+/*   Updated: 2026/02/09 04:45:52 by ehode            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// This class will replace the std::vector<int> _client_sockets in servers
+// It will contain:
+// - Socket
+// - Raw IP Address
+// - IP Address
+// - Last receive request
+// - Total request
+class Client {};
